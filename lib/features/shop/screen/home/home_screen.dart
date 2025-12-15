17cfba8ceaf0a000dd1ebb9db57b4012b560d7d3
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2025-12-16 01:05:31 +0600

    Add Promotion Banner

--- lib/features/shop/screen/home/home_screen.dart
+++ lib/features/shop/screen/home/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:shopping_app_with_getx/common/widgets/textfields/search_bar.dart';
+import 'package:shopping_app_with_getx/features/shop/controller/home/home_controller.dart';
 import 'package:shopping_app_with_getx/features/shop/screen/home/widget/home_appbar.dart';
 import 'package:shopping_app_with_getx/features/shop/screen/home/widget/home_categories.dart';
 import 'package:shopping_app_with_getx/features/shop/screen/home/widget/primary_header_container.dart';
+import 'package:shopping_app_with_getx/features/shop/screen/home/widget/promo_slider.dart';
+import 'package:shopping_app_with_getx/utils/constants/images.dart';
 import 'package:shopping_app_with_getx/utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,31 +14,50 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    // ignore: unused_local_variable
+    final controller = Get.put(HomeController());
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          SizedBox(height: AppSizes.homePrimaryHeaderHeight + 20),
+          // ------[Upper part]-----
+          Stack(
+            children: [
+              SizedBox(height: AppSizes.homePrimaryHeaderHeight + 20),
 
-          // primary header container
-          AppPrimaryHeaderContainer(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // appbar
-                AppHomeAppBar(),
+              // primary header container
+              AppPrimaryHeaderContainer(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // appbar
+                    AppHomeAppBar(),
 
-                SizedBox(height: AppSizes.spaceBtwSections),
+                    SizedBox(height: AppSizes.spaceBtwSections),
 
-                // new category
-                AppHomeCategories(),
-              ],
-            ),
+                    // new category
+                    AppHomeCategories(),
+                  ],
+                ),
+              ),
+
+              // search bar
+              AppSearchBar(),
+
+              //
+            ],
           ),
 
-          // search bar
-          AppSearchBar(),
-
-          //
+          // ------[Lower part]-----
+          Padding(
+            padding: const EdgeInsets.all(AppSizes.defaultSpace),
+            child: AppPromoSlider(banners: [
+              AppImages.banner1,
+              AppImages.banner2,
+              AppImages.banner3,
+              AppImages.banner4,
+              ]),
+          ),
         ],
       ),
     );

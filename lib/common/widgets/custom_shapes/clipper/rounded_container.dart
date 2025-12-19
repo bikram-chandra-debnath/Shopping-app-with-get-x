880ea93a993633842bb1a60a1aa35f8cb4b512d2
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2025-12-19 23:26:47 +0600

    fix circular container

---
+++ lib/common/widgets/custom_shapes/clipper/rounded_container.dart
@@ -0,0 +1,36 @@
+
+
+import "package:flutter/material.dart";
+import "package:shopping_app_with_getx/utils/constants/colors.dart";
+
+class AppRoundedContainer extends StatelessWidget {
+  const AppRoundedContainer({
+    super.key,
+     this.height = 400,
+     this.width= 400,
+     this.backgroundColor= AppColors.white,
+     this.padding,
+     this.margin, this.child,
+  });
+
+  final double height, width;
+
+  final Color backgroundColor;
+  final EdgeInsetsGeometry? padding, margin;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      padding: padding,
+      margin: margin,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(1000),
+        color: backgroundColor,
+      ),
+      child: child,
+    );
+  }
+}

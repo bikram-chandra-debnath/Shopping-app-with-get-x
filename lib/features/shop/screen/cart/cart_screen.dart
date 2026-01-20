650017bdd00c9c066a6c7552a903aefdbc5f36b2
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-01-20 20:57:02 +0600

    remove unused imports

--- lib/features/shop/screen/cart/cart_screen.dart
+++ lib/features/shop/screen/cart/cart_screen.dart
@@ -1,27 +1,19 @@
 import "package:flutter/material.dart";
-import "package:iconsax/iconsax.dart";
 import "package:shopping_app_with_getx/common/style/padding.dart";
 import "package:shopping_app_with_getx/common/widgets/appbar/my_appbar.dart";
 import "package:shopping_app_with_getx/common/widgets/buttons/app_elevated_button.dart";
-import "package:shopping_app_with_getx/common/widgets/icons/circular_icons.dart";
-import "package:shopping_app_with_getx/common/widgets/images/rounded_image.dart";
-import "package:shopping_app_with_getx/common/widgets/text/brand_title_with_verify_icon.dart";
 import "package:shopping_app_with_getx/common/widgets/text/product_price.dart";
-import "package:shopping_app_with_getx/common/widgets/text/product_title_text.dart";
-import "package:shopping_app_with_getx/utils/constants/colors.dart";
-import "package:shopping_app_with_getx/utils/constants/images.dart";
 import "package:shopping_app_with_getx/utils/constants/sizes.dart";
 
 import "../../../../common/widgets/products/cart/cart_item.dart";
 import "../../../../common/widgets/products/cart/product_quantity_with_add_remove.dart";
-import "../../../../utils/helpers/helper_function.dart";
+
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dark = AppHelperFunction.isDarkMode(context);
     return Scaffold(
       //-----[AppBar]------
       appBar: MyAppBar(

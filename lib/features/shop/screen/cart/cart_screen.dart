a35582347695bb40b132fc8d540c714625a32dda
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-01-20 21:16:35 +0600

    make page reuseable

--- lib/features/shop/screen/cart/cart_screen.dart
+++ lib/features/shop/screen/cart/cart_screen.dart
@@ -2,11 +2,9 @@ import "package:flutter/material.dart";
 import "package:shopping_app_with_getx/common/style/padding.dart";
 import "package:shopping_app_with_getx/common/widgets/appbar/my_appbar.dart";
 import "package:shopping_app_with_getx/common/widgets/buttons/app_elevated_button.dart";
-import "package:shopping_app_with_getx/common/widgets/text/product_price.dart";
+import "package:shopping_app_with_getx/features/shop/screen/cart/widgets/cart_items.dart";
 import "package:shopping_app_with_getx/utils/constants/sizes.dart";
 
-import "../../../../common/widgets/products/cart/cart_item.dart";
-import "../../../../common/widgets/products/cart/product_quantity_with_add_remove.dart";
 
 
 class CartScreen extends StatelessWidget {
@@ -24,36 +22,7 @@ class CartScreen extends StatelessWidget {
       //-----[body]------
       body: Padding(
         padding: AppPadding.screenPadding,
-        child: ListView.separated(
-          separatorBuilder:
-              (BuildContext context, int index) =>
-                  SizedBox(height: AppSizes.spaceBtwSections),
-          itemCount: 13,
-          itemBuilder: (context, index) {
-            return Column(
-              children: [
-                // cart item
-                AppCartItem(),
-                SizedBox(height: AppSizes.spaceBtwItems),
-                // price, counter button
-                Row(
-                  children: [
-                    // extra space
-                    SizedBox(width: 70),
-
-                    // quantity button
-                    AppProductQuantityWithAddRemove(),
-
-                    Spacer(),
-
-                    // price text
-                    AppProductPrice(price: "323"),
-                  ],
-                ),
-              ],
-            );
-          },
-        ),
+        child: AppCartItems( showAddRemoveButtons: true,),
       ),
 
       // bottom navigation bar
---
+++ lib/features/shop/screen/cart/widgets/cart_items.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../common/widgets/products/cart/cart_item.dart';
+import '../../../../../common/widgets/products/cart/product_quantity_with_add_remove.dart';
+import '../../../../../common/widgets/text/product_price.dart';
+import '../../../../../utils/constants/sizes.dart';
+
+
+class AppCartItems extends StatelessWidget {
+   AppCartItems({
+    super.key,
+    required this.showAddRemoveButtons,
+  });
+
+  bool showAddRemoveButtons;
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      separatorBuilder:
+          (BuildContext context, int index) =>
+          SizedBox(height: AppSizes.spaceBtwSections),
+      itemCount: 13,
+      itemBuilder: (context, index) {
+        return Column(
+          children: [
+            // cart item
+            AppCartItem(),
+            if (showAddRemoveButtons)SizedBox(height: AppSizes.spaceBtwItems),
+            // price, counter button
+            if (showAddRemoveButtons)Row(
+              children: [
+                // extra space
+                SizedBox(width: 70),
+
+                // quantity button
+                AppProductQuantityWithAddRemove(),
+
+                Spacer(),
+
+                // price text
+                AppProductPrice(price: "323"),
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+}

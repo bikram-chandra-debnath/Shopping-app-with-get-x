f7a27d4449c25bb962eb823d2453ab25b9243476
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-01-21 16:30:21 +0600

    make success screen dynamic

--- lib/common/widgets/screen/success_screen.dart
+++ lib/common/widgets/screen/success_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app_with_getx/common/style/padding.dart';
 import 'package:shopping_app_with_getx/common/widgets/buttons/app_elevated_button.dart';
-import 'package:shopping_app_with_getx/utils/constants/images.dart';
+
 import 'package:shopping_app_with_getx/utils/constants/sizes.dart';
 import 'package:shopping_app_with_getx/utils/constants/texts.dart';
 import 'package:shopping_app_with_getx/utils/helpers/device_helpers.dart';
 
 class SuccessScreen extends StatelessWidget {
-  const SuccessScreen({super.key});
+  const SuccessScreen({super.key, required this.title, required this.subTitle, required this.image, required this.onTap});
+
+  final String title,subTitle, image;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
@@ -21,21 +24,24 @@ class SuccessScreen extends StatelessWidget {
             children: [
               // image
               Image.asset(
-                AppImages.accountCreateSuccessful,
+                image,
                 height: AppDeviceHelper.getScreenWidth(context) * 0.6,
               ),
+              SizedBox(height: AppSizes.spaceBtwItems),
 
               // title
               Text(
-                AppTexts.accountCreatedSuccessTitle,
+                title,
                 style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
               ),
               SizedBox(height: AppSizes.spaceBtwItems),
 
               // Subtitle
               Text(
-                AppTexts.accountCreatedSuccessSubtitle,
+                subTitle,
                 style: Theme.of(context).textTheme.bodySmall,
+                textAlign: TextAlign.center,
               ),
               SizedBox(height: AppSizes.spaceBtwSections),
 

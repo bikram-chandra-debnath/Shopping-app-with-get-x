571270cfad76b181a5535d8ba24b60c6ef4f8310
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-01-31 00:33:47 +0600

    Add project with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shopping_app_with_getx/firebase_options.dart';
 import 'package:shopping_app_with_getx/my_app.dart';
 
-void main() {
+Future<void> main () async{
   /// widgets flutter binding
   WidgetsFlutterBinding.ensureInitialized();
   // flutter native splash
  // FlutterNativeSplash.preserve(widgetsBinding: WidgetsBinding);
-
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
  // portrait up the device
  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());

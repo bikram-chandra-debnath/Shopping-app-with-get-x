625b2f7abed18ea9b6f1132dc784e0ca3831ff12
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-01-30 23:03:48 +0600

    fix Navigation

--- lib/common/widgets/brands/brand_card.dart
+++ lib/common/widgets/brands/brand_card.dart
@@ -10,43 +10,47 @@ import 'package:shopping_app_with_getx/utils/constants/texts.dart';
 
 class AppBrandCard extends StatelessWidget {
   const AppBrandCard({
-    super.key,  this.showBorder=true,
+    super.key,  this.showBorder=true, this.onTap,
   });
   final bool showBorder;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return AppRoundedContainer(
-      height:AppSizes.brandCardHeight,
-     
-      showBorder:showBorder,
-      padding: EdgeInsets.all(AppSizes.sm),
-      backgroundColor: Colors.transparent,
-      child: Row(
-        children: [
-          Flexible(child: AppRoundedimage(imageUrl: AppImages.bata,backgroundColor: Colors.transparent,)),
-          SizedBox(width: AppSizes.spaceBtwItems / 2),
-    
-          // brand name and verify icon
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                AppBrandTitleWithVerifyIcon(
-                  title: AppTexts.bata,
-                  brandTextSize: TextSizes.large,
-                ),
-                
-                Text(
-                  "172 products",
-                  style: Theme.of(context).textTheme.labelMedium,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
+    return GestureDetector(
+      onTap: onTap,
+      child: AppRoundedContainer(
+        height:AppSizes.brandCardHeight,
+       
+        showBorder:showBorder,
+        padding: EdgeInsets.all(AppSizes.sm),
+        backgroundColor: Colors.transparent,
+        child: Row(
+          children: [
+            Flexible(child: AppRoundedimage(imageUrl: AppImages.bata,backgroundColor: Colors.transparent,)),
+            SizedBox(width: AppSizes.spaceBtwItems / 2),
+      
+            // brand name and verify icon
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  AppBrandTitleWithVerifyIcon(
+                    title: AppTexts.bata,
+                    brandTextSize: TextSizes.large,
+                  ),
+                  
+                  Text(
+                    "172 products",
+                    style: Theme.of(context).textTheme.labelMedium,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
